eb81d2738a81f909b7aff85a9e0ade062ad06726
Vlad <vladdp70@gmail.com>
2021-05-25 03:42:55 +0300

    Повтор классов

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-'use strict';
+"use strict";
+
+class Animal {
+  constructor(name, amountOfLegs, type) {
+    this._name = name;
+    this._amountOfLegs = amountOfLegs;
+    this._type = type;
+  }
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === "string") {
+      this._name = name;
+    }
+  }
+  eat() {
+    return `${this.name} is eating`;
+  }
+}
+
+const lion = new Animal("sneg", 4, "Cat");
+const turtle = new Animal("test", 4, "Turtle");
